a1a6f037e77a3802836ff23152f8626ddec12ada
NIck1821 <personallune@mail.ru>
2020-09-20 16:47:40 +0400

    commit.4

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -7,23 +7,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import web.Model.Car;
 import web.Service.CarService;
 
-import java.util.ArrayList;
 import java.util.List;
+
 
 @Controller
 public class CarController {
 
     @GetMapping(value = "/cars")
-    public String printWelcome(@RequestParam(value = "locale", required = false) String param,
-                               ModelMap model) {
+    public String printWelcome(ModelMap model) {
         List<Car> carModel = new CarService().carList();
-        if (param.contains( "ru")){
-            param = "Машины";
-        } else {
-            param = "Cars";
-        }
         model.addAttribute("CarsModel", carModel);
-        model.addAttribute("Param", param);
         return "cars";
     }
 }
